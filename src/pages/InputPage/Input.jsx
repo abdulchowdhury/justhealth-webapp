c66567c8acc5b309b2fdbfad9d2bc78181bf315c
Christian <ckim466@gatech.edu>
2022-10-08 16:37:56 -0400

    remove balls

--- src/pages/InputPage/Input.jsx
+++ src/pages/InputPage/Input.jsx
@@ -10,7 +10,7 @@ const Input = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    alert(`balls balls balls balls\n
+    alert(`
      procedure: ${procedure}\n
      cost: ${cost}\n 
      hospital: ${hospital}\n
