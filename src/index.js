e6f058eb2d81b0dd6f218702b9c3c5f326bde008
Christian <ckim466@gatech.edu>
2022-10-08 12:00:16 -0400

    Fix routing, add MUI

--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
-import Home from "./pages/Home";
-import Blogs from "./pages/Blogs";
-import Contact from "./pages/Contact";
+import Home from "./pages/Home/Home";
+
 import NoPage from "./pages/NoPage";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <div>
+      <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="blogs" element={<Blogs />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} />
-        </Route>
+        <Route index element={<Home />} />
+        <Route path="/layout" element={<Layout />} />
+        <Route path="*" element={<NoPage />} />
       </Routes>
     </BrowserRouter>
+    </div>
   );
 }
 
