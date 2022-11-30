726c1fbfd6c8fc6d688541b05c89280f6bb4748f
Celestin <chering6@gatech.edu>
2022-11-30 01:33:59 -0500

    lets try this again

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ connection.connect(function(err) {
     if(err) {
         console.log("Error: ", err);
     } else {
-        console.log("Server connection established");
+        console.log("Connection established");
     }
 })
 
